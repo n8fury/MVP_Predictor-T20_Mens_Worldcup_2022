a7135f41cd02cea2163eac912b2258f20f4db6b1
n8fury <mhjoy547@yahoo.com>
2023-01-10 23:33:40 +0600

    added home route and prediction route

--- main.py
+++ main.py
@@ -9,3 +9,20 @@ app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
 with open('scaler.pkl', 'rb') as f:
     scalerO = pickle.load(f)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/predict_api', methods=['POST'])
+def results():
+    data = request.get_json(force=True)
+    prediction = model.predict([np.array(list(data.values()))])
+    output = prediction[0]
+    return jsonify(output)
+
+
+if __name__ == "__main__":
+    app.run(debug=False)
