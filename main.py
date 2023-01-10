5b236ea1294014f478fe90daec86332dc996d587
n8fury <mhjoy547@yahoo.com>
2023-01-10 23:32:25 +0600

    created Flask app & loaded model and scaler

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ import pickle
 import numpy as np
 import warnings
 warnings.filterwarnings("ignore")
+
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+with open('scaler.pkl', 'rb') as f:
+    scalerO = pickle.load(f)
