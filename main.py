b3064137d4a49e67384871b17a5c30e4f888d70d
n8fury <mhjoy547@yahoo.com>
2023-01-11 20:39:10 +0600

    optimized inputs for ML model

--- main.py
+++ main.py
@@ -25,7 +25,17 @@ def predict():
     wickets = float(request.form['wickets'])
     economy = float(request.form['economy'])
     semiprobability = float(request.form['semiprobability'])
-
+    sm = semiprobability / 100.00
+    keyplayer = request.form['keyplayer']
+    batting_impact = ((battingavg*strikerate)/160.00)/100.00
+    rbi = round(batting_impact, 2)
+    wicketpermatch = wickets/totalmatches
+    rwpm = round(wicketpermatch, 2)
+    ew = economy*wicketpermatch
+    if ew == 0:
+        bowling_impact = 0.00
+    else:
+        bowling_impact = (96.00/(ew))/100.00
 
 
 
